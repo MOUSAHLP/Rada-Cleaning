e431e0b87237e5ff60681aa82555500d89e502e4
Issa-Al-Halabi <86517770+MOUSAHLP@users.noreply.github.com>
2023-01-24 11:43:31 +0300

    update

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let serviceItems = document.querySelectorAll(".service-item"); 
+serviceItems.forEach(
+    (serviceItem)=>{
+        serviceItem.onclick=  ()=>{
+              document.getElementById("contact").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+        }
+    }
+)
